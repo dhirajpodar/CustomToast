d7815f3636e357ecfc29aeb5471c59d9e3515f7f
Dhiraj Poddar <dhiraj.poddar@citytech.global>
2020-05-18 14:01:41 +0545

    set background programmtically

--- app/src/main/java/com/example/customtoast/MainActivity.java
+++ app/src/main/java/com/example/customtoast/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.customtoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -9,13 +10,15 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.view.Gravity.*;
 
 public class MainActivity extends AppCompatActivity {
   Button showToast;
   TextView messageText;
-  private boolean tapped = false;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
     showToast = findViewById(R.id.btn_toast);
     final View view = LayoutInflater.from(this).inflate(R.layout.custom_toast1, null, false);
     messageText = view.findViewById(R.id.message_text);
+    LinearLayout linearLayout = view.findViewById(R.id.ll_toast);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      linearLayout.setBackground(getDrawable(R.drawable.shape));
+    }
     String message = "I am the message.";
     messageText.setText(message);
     return view;
@@ -47,16 +54,18 @@ public class MainActivity extends AppCompatActivity {
     toast.setView(view);
     int width = displayMetrics.widthPixels;
     Log.d("TAG","Width::"+width);
+    Log.d("TAG","y - Off::"+toast.getYOffset());
+    Log.d("TAG","x-off::"+toast.getXOffset());
     if (displayDpi < 480) {
-      toast.setGravity(Gravity.BOTTOM, getXOffsetForToast(width, (float) 0.84), 64);
+      toast.setGravity(Gravity.BOTTOM| LEFT, getXOffsetForToast(width, (float) 0.84), 64);
     } else if (displayDpi < 599) {
-      toast.setGravity(Gravity.BOTTOM, getXOffsetForToast(width, (float) 0.68), 64);
+      toast.setGravity(Gravity.BOTTOM| LEFT, getXOffsetForToast(width, (float) 0.68), 64);
     } else if (displayDpi >= 600 && displayDpi < 959) {
-      toast.setGravity(Gravity.BOTTOM, getXOffsetForToast(width, (float) 0.68), 64);
+      toast.setGravity(Gravity.BOTTOM| LEFT, getXOffsetForToast(width, (float) 0.68), 64);
     } else if (displayDpi >= 960 && displayDpi < 1919) {
-      toast.setGravity(Gravity.BOTTOM, getXOffsetForToast(width, (float) 0.37), 64);
+      toast.setGravity(Gravity.BOTTOM| LEFT, getXOffsetForToast(width, (float) 0.37), 64);
     } else {
-      toast.setGravity(Gravity.BOTTOM, getXOffsetForToast(width, (float) 0.25), 64);
+      toast.setGravity(Gravity.BOTTOM| LEFT, getXOffsetForToast(width, (float) 0.25), 64);
     }
     toast.show();
 
